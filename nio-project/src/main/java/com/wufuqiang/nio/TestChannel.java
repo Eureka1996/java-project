f14c1e12a97139977fd0df727deb4330b336c089
wufuqiang <wufuqiang@buaa.edu.cn>
2020-12-25 19:28:31 +0800

    ADD：增加注释

--- nio-project/src/main/java/com/wufuqiang/nio/TestChannel.java
+++ nio-project/src/main/java/com/wufuqiang/nio/TestChannel.java
@@ -1,13 +1,12 @@
 package com.wufuqiang.nio;
 
-import com.sun.javafx.image.ByteToBytePixelConverter;
 import org.junit.Test;
-
 
 import java.io.IOException;
 import java.io.RandomAccessFile;
 import java.nio.ByteBuffer;
 import java.nio.CharBuffer;
+import java.nio.channels.Channels;
 import java.nio.channels.FileChannel;
 import java.nio.charset.CharacterCodingException;
 import java.nio.charset.Charset;
@@ -16,24 +15,27 @@ import java.nio.charset.CharsetEncoder;
 
 public class TestChannel {
 
-	//编码、解码
+	//buffer的操作，编码、解码
 	@Test
 	public void test5() throws CharacterCodingException {
+		//设置字符集
 		Charset gdk = Charset.forName("GBK");
 		CharsetEncoder charsetEncoder = gdk.newEncoder();
 		CharsetDecoder charsetDecoder = gdk.newDecoder();
 
+		//往charBuffer中放入数据
 		CharBuffer cBuf = CharBuffer.allocate(100);
 		cBuf.put("吴福强");
 		cBuf.flip();
 
-		ByteBuffer encode = charsetEncoder.encode(cBuf);
-		for(int i = 0 ; i<encode.limit();i++){
-			System.out.println(encode.get());
+		//将charBuffer编码成byteBuffer
+		ByteBuffer bBuf = charsetEncoder.encode(cBuf);
+		for(int i = 0 ; i<bBuf.limit();i++){
+			System.out.println(bBuf.get());
 		}
-		encode.flip();
-		CharBuffer decode = charsetDecoder.decode(encode);
-		System.out.println(decode.toString());
+		bBuf.flip();
+		CharBuffer decodeCBuf = charsetDecoder.decode(bBuf);
+		System.out.println(decodeCBuf.toString());
 
 
 	}
@@ -50,6 +52,10 @@ public class TestChannel {
 
 		channel1.read(bufArr);
 		for(ByteBuffer buf: bufArr){
+			//flip()：设置limit为position的值，然后position置为0。对Buffer进行读取操作前调用
+			//rewind()：仅仅将positionl置0。一般是在重新读取buffer数据前调用，比如要读取同一个buffer的数据写入多个通道时会用到
+			//clear()：回到初始状态，即limit等于capacity，position置0。重新对Buffer进行写入操作前调用
+			//compact()：将未读取完的数据移动到缓冲区开关，交将position设置为这段数据末尾的下一个位置。其实就等价于重新向缓冲区写入这一段数据
 			buf.flip();
 		}
 		System.out.println(new String(buf1.array(),0,buf1.limit()));
